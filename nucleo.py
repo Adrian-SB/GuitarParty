dc36f9f25d59fac0786736086c0059a1af496521
Adrian-SB <asboceta@gmail.com>
2017-05-15 12:33:21 +0200

    kernel1.0
    Primera aproximación

---
+++ nucleo.py
@@ -0,0 +1,53 @@
+
+import requests
+import json
+
+headers={'Guitarparty-Api-Key': '6115b4a078d150136759b6e1b85dfba83b68190b'}
+url='http://api.guitarparty.com/v2/'
+
+@route('/', method="get")
+def intro():
+	return template('template.tpl')
+
+#Busqueda por artista
+
+@route('/biografia', method="post")
+def artista():
+nombre="Jimi"
+r=requests.get(url+'artists/?query='+nombre,headers=headers)
+if r.status_code == 200:
+    text=r.json()
+ 	 nombrecompleto =  text["objects"][0]["name"]
+ 	 bigrafia = text["objects"][0]["bio"]
+
+#Busqueda por canciones
+
+@router('/canciones')
+def canciones():
+cancion="Jolene"
+r=requests.get(url+'songs/?query='+cancion,headers=headers)
+if r.status_code == 200:
+    text=r.json()
+    print text["objects"][0]["title"]
+    print text["objects"][0]["uri"]
+    print text["objects"][0]["chords"]
+    print text["objects"][0]["authors"][1]["types"]
+    print text["objects"][0]["authors"][1]["name"]
+    print text["objects"][0]["authors"][1]["uri"]
+
+#Muestra los acordes
+
+@router('/acordes')
+def acorde():
+acorde="Am"
+r=requests.get(url+'chords/?query='+acorde,headers=headers)
+if r.status_code == 200:
+    text=r.json()
+    print 'Acorde: ', text["objects"][0]["name"]
+    print 'Instrumento: ',text["objects"][0]["instrument"]["name"]
+    print 'Afinacion: ',text["objects"][0]["instrument"]["tuning"]
+    print 'Imagen: ',  text["objects"][0]["image_url"]
+ 
+
+if __name__ == '__main__':
+	run(host='0.0.0.0',port=argv[1])
